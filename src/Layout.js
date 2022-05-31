0b2fbff41d8f804f3ebb5e909dea564649ecab1e
gordielachance <gordielachance@users.noreply.github.com>
2022-05-31 19:43:55 +0200

    "mobile" class for #layout

--- src/Layout.js
+++ src/Layout.js
@@ -67,6 +67,7 @@ function Layout() {
   const location = useLocation();
   const screenSize = useWindowDimensions();
   const [vertical,setVertical] = useState();
+  const [mobile,setMobile] = useState();
 
   const NotFound = () => (
     <h2>404 Page Not Found</h2>
@@ -74,15 +75,18 @@ function Layout() {
 
   //check is vertical
   useEffect(()=>{
-    const bool = screenSize.height > screenSize.width;
-    setVertical(bool);
+    const isVertical = screenSize.height > screenSize.width;
+    const isMobile = screenSize.width <= 576;
+    setVertical(isVertical);
+    setMobile(isMobile);
   },[screenSize]);
 
   return (
     <div
     id="layout"
     className={classNames({
-      vertical: vertical
+      vertical: vertical,
+      mobile: mobile
     })}
     >
       <div id="site-logo">
@@ -90,20 +94,20 @@ function Layout() {
           <img src="https://www.tribunaldesprejuges.org/wordpress/wp-content/themes/tribunaldesprejuges/_inc/images/logo-tdp.png"/>
         </Link>
       </div>
-     <Routes location={location}>
-        <Route path="/" element={<PageHome />} />
-        <Route path="/agenda" element={<PageAgenda/>} />
-        <Route path="/creations" element={<PageCreations/>} />
-        <Route path="/credits" element={<PageCredits/>} />
-        <Route path="/cartes">
-          <Route index element={<PageMaps />} />
-          <Route path=":mapPostId/:mapPostSlug" element={<PageSingleMap />} />
-          <Route path=":mapPostId/:mapPostSlug/:urlSourceId/:urlFeatureId" element={<PageSingleMap />} />
-          <Route path=":mapPostId/:mapPostSlug/:urlSourceId/:urlFeatureId/:urlFeatureAction" element={<PageSingleMap />} />
-        </Route>
+       <Routes location={location}>
+          <Route path="/" element={<PageHome />} />
+          <Route path="/agenda" element={<PageAgenda/>} />
+          <Route path="/creations" element={<PageCreations/>} />
+          <Route path="/credits" element={<PageCredits/>} />
+          <Route path="/cartes">
+            <Route index element={<PageMaps />} />
+            <Route path=":mapPostId/:mapPostSlug" element={<PageSingleMap />} />
+            <Route path=":mapPostId/:mapPostSlug/:urlSourceId/:urlFeatureId" element={<PageSingleMap />} />
+            <Route path=":mapPostId/:mapPostSlug/:urlSourceId/:urlFeatureId/:urlFeatureAction" element={<PageSingleMap />} />
+          </Route>
 
-        <Route path='*' component={NotFound} />
-      </Routes>
+          <Route path='*' component={NotFound} />
+        </Routes>
     </div>
   );
 }
