8397f0f024db4dff38abe07fb0917f95d9f18294
gordielachance <gordie.lachance@gmail.com>
2023-09-01 11:22:00 +0200

    getPaginatedItems() no more needed since we changed the way the backend handles rest responses

--- src/databaseAPI/api.js
+++ src/databaseAPI/api.js
@@ -4,8 +4,8 @@ import {databaseAPI} from "./connect";
 
 export default class DatabaseAPI extends React.Component {
 
-  static async getPaginatedItems(type, params = {}) {
-    const defaultParams = { page: 1, per_page: 10 };
+  static async getItems(type, params = {}) {
+    const defaultParams = {};
     const mergedParams = { ...defaultParams, ...params };
 
     console.info(`GET '${type}' ITEMS,`,mergedParams);
@@ -18,53 +18,6 @@ export default class DatabaseAPI extends React.Component {
     return databaseAPI.request(config)
   }
 
-  static async getItems(type, params = {}) {
-    const defaultParams = { page: 1, per_page: 10 };
-    const mergedParams = { ...defaultParams, ...params };
-
-    let items = [];
-
-    console.info(`GET '${type}' ITEMS`,mergedParams);
-
-    while (true) {
-      try {
-        const request = await DatabaseAPI.getPaginatedItems(type,mergedParams);
-        const pageItems = request.data;
-
-        if (pageItems.length === 0) {
-          // No more items to fetch
-          break;
-        }
-
-        items = items.concat(pageItems);
-        mergedParams.page++;
-      } catch (error) {
-        console.error(`ERROR GETTING '${type}' ITEMS`, type,mergedParams,error);
-        break; // Exit the loop on error
-      }
-    }
-
-    //merge paginated feature into a single collection
-    if (type === 'features' && params.geojson){
-
-      const mergePaginatedCollections = pages => {
-          const features = [];
-
-          for (const collection of pages) {
-              features.push(...collection.features);
-          }
-
-          return {
-              type: 'FeatureCollection',
-              features: features
-          };
-      }
-
-      items = mergePaginatedCollections(items);
-    }
-
-    return items;
-  }
 
   static async getSingleItem(type,id,params){
 
